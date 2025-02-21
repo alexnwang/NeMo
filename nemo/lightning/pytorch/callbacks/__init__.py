6926c51f2230d8adcbe29d23d6f708dace5b3086
alex wang <anw2067@nyu.edu>
2025-02-21 20:28:25 +0000

    Shows step time during trianing

--- nemo/lightning/pytorch/callbacks/__init__.py
+++ nemo/lightning/pytorch/callbacks/__init__.py
@@ -25,6 +25,7 @@ from nemo.lightning.pytorch.callbacks.peft import PEFT
 from nemo.lightning.pytorch.callbacks.preemption import PreemptionCallback
 from nemo.lightning.pytorch.callbacks.progress_bar import MegatronProgressBar
 from nemo.lightning.pytorch.callbacks.progress_printer import ProgressPrinter
+from nemo.lightning.pytorch.callbacks.step_timer_callback import StepTimerCallback
 
 __all__ = [
     "MemoryProfileCallback",
@@ -41,4 +42,5 @@ __all__ = [
     "ModelCallback",
     "JitTransform",
     "JitConfig",
+    "StepTimerCallback",
 ]
